802bdaee26bbc53f4e3345471a6d456c1dbf25eb
anant911 <77596400+anant911@users.noreply.github.com>
2021-05-17 19:50:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var wak = [
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F4b%2F92%2F8b%2F4b928be7d4b2d54c72227350709adebd.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F251920172885345448%2F&tbnid=9y4t2eMxNAoMzM&vet=12ahUKEwiJ0JXIq8fwAhXNHN8KHVFpCL0QMygAegUIARC7AQ..i&docid=Y8ekM3Zeh883ZM&w=517&h=420&q=funny%20pics%20of%20monkeys&safe=active&ved=2ahUKEwiJ0JXIq8fwAhXNHN8KHVFpCL0QMygAegUIARC7AQ",
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmir-s3-cdn-cf.behance.net%2Fproject_modules%2Fdisp%2F27672c11476433.560f852433b46.jpg&imgrefurl=https%3A%2F%2Fwww.behance.net%2Fgallery%2F11476433%2FFunny-Monkeys&tbnid=-6luVN_6HKpXrM&vet=12ahUKEwiJ0JXIq8fwAhXNHN8KHVFpCL0QMygKegUIARDTAQ..i&docid=HZ1Tkfvg94FDbM&w=600&h=850&q=funny%20pics%20of%20monkeys&safe=active&ved=2ahUKEwiJ0JXIq8fwAhXNHN8KHVFpCL0QMygKegUIARDTAQ",
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F12%2FGettyImages-78777891-scaled.jpg&imgrefurl=https%3A%2F%2Fwww.rd.com%2Flist%2Ffunny-monkey-pictures%2F&tbnid=vekgxRKEHaaTDM&vet=12ahUKEwiJ0JXIq8fwAhXNHN8KHVFpCL0QMygIegUIARDPAQ..i&docid=eblyQb1cD1q4_M&w=2560&h=1704&q=funny%20pics%20of%20monkeys&safe=active&ved=2ahUKEwiJ0JXIq8fwAhXNHN8KHVFpCL0QMygIegUIARDPAQ",
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fst2.depositphotos.com%2F2927537%2F7025%2Fi%2F600%2Fdepositphotos_70256441-stock-photo-funny-monkey-with-a-red.jpg&imgrefurl=https%3A%2F%2Fdepositphotos.com%2Fstock-photos%2Ffunny-monkey.html&tbnid=EIWAT1Jo0QMSoM&vet=12ahUKEwiJ0JXIq8fwAhXNHN8KHVFpCL0QMygPegUIARDfAQ..i&docid=JO02vfx_mXt2VM&w=600&h=400&q=funny%20pics%20of%20monkeys&safe=active&ved=2ahUKEwiJ0JXIq8fwAhXNHN8KHVFpCL0QMygPegUIARDfAQ",
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D698547680506088&imgrefurl=https%3A%2F%2Fwww.facebook.com%2FFunnyMonkey0%2F&tbnid=sIFulDlRU7-1-M&vet=12ahUKEwiJ0JXIq8fwAhXNHN8KHVFpCL0QMygSegUIARDnAQ..i&docid=HA3ccF4iV_XIqM&w=861&h=861&q=funny%20pics%20of%20monkeys&safe=active&ved=2ahUKEwiJ0JXIq8fwAhXNHN8KHVFpCL0QMygSegUIARDnAQ",
+    
+  ];
+    
+  
+  var i=0;
+  
+  function move() {
+    document.getElementById("monkey").src=wak[i];
+    i++;
+    if(i==6)
+      {i=0;
+      }
+  }
+  
